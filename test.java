9a1a46166a76eda189cfbf337876e35defd9b022
leslie-fang <Leslie-fang92>
2018-02-27 05:19:29 +0800

    test jenkinsing

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
 public class test
 {
   public static void main(String[] args) {
-     System.out.println("Hello Jenkins, hew is leslie!");
+     System.out.println("Hello Jenkins, I am leslie!");
   }
 }
